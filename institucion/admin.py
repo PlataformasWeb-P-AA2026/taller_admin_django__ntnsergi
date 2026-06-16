ee8bb5d4f6fba16292d70c67984657ac2015a0ac
ntnsergi <sergi.galarzaend@gmail.com>
2026-06-16 12:16:28 -0500

    v2

--- institucion/admin.py
+++ institucion/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Museo, GuiaMuseo, Exhibicion
+
+class MuseoAdmin(admin.ModelAdmin):
+    list_display = (
+        'nombre', 
+        'ciudad', 
+        'anio_fundacion', 
+        'costo_total_produccion',       
+        'guias_mas_experimentados'       
+    )
+
+class GuiaMuseoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_completo', 'anios_experiencia_guia', 'idiomas_hablados', 'museo')
+    
+
+class ExhibicionAdmin(admin.ModelAdmin):
+    list_display = ('titulo_exhibicion', 'duracion_meses', 'costo_produccion', 'tematica', 'guia')
--- institucion/models.py
+++ institucion/models.py
@@ -8,7 +8,23 @@ class Museo(models.Model):
     anio_fundacion = models.IntegerField("Año de fundacion")
     
     def __str__(self):
-        return "%s %s %f" % (self.nombre, self.ciudad , self.anio_fundacion)
+        return "%s %s %d" % (self.nombre, self.ciudad , self.anio_fundacion)
+    
+    def costo_total_produccion(self):
+        total = 0
+        for guia in self.guias.all():
+            for exhibicion in guia.exhibiciones.all():
+                total += exhibicion.costo_produccion
+        
+        return total
+    
+    def guias_mas_experimentados(self):
+        mejor_guia = self.guias.all().order_by('-anios_experiencia_guia').first()
+        
+        if mejor_guia:
+            return mejor_guia.nombre_completo
+        else:
+            return "No hay texto"
 
 class GuiaMuseo(models.Model):
     nombre_completo = models.CharField("Nombre y Apellido del guia")
@@ -17,7 +33,9 @@ class GuiaMuseo(models.Model):
     
     museo = models.ForeignKey(Museo, on_delete=models.CASCADE, related_name="guias")
     
-    
+    def __str__(self):
+        return "%s %d %s " % (self.nombre_completo, self.anios_experiencia_guia,\
+            self.idiomas_hablados)
     
 class Exhibicion(models.Model):
     titulo_exhibicion = models.CharField("Titulo de la exhibicion")
@@ -25,5 +43,10 @@ class Exhibicion(models.Model):
     costo_produccion = models.FloatField("Costo total de la produccion")
     tematica = models.CharField("Tematica proncipal de la exhibicion")
     
-    guia = models.ForeignKey(GuiaMuseo, on_delete=models.CASCADE, related_name="exhibiciones")
+    guia = models.ForeignKey(GuiaMuseo, on_delete=models.CASCADE, \
+        related_name="exhibiciones")
+    
+    def __str__(self):
+        return "%s %d %f %s " % (self.titulo_exhibicion, self.duaracion_meses,\
+            self.costo_produccion, self.tematica)
     
